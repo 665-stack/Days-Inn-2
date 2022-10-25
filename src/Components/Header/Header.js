9f34cd61cac34f70db41ba14164bfcdaf6bbc16d
MahmodulShahin <mahmodulshahin665@gmail.com>
2022-10-25 20:05:03 +0600

    add header, header style and a little responsiveness

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -3,15 +3,21 @@ import { Link } from 'react-router-dom';
 import './Header.css'
 const Header = () => {
     return (
-        <header>
-            <Link to='/'>Days Inn</Link>
-            <nav>
-                <Link to='/'>Home</Link>
-                <Link to='/about-us'>About Us</Link>
-                <Link to='/rooms'>Rooms</Link>
-                <Link to='/contact'>Contact</Link>
-                <Link to='/login'>Login</Link>
-                <Link to='/register'>Register</Link>
+        <header className='flex justify-center md:justify-between items-center	 bg-neutral-900 sm:px-12 md:px-20 px-2 py-5'>
+            <Link className='uppercase text-slate-50 text-xl hidden md:block' to='/'>Days Inn</Link>
+            <nav className='flex  text-slate-50 hover:text-slate-200 text-lg'>
+
+                <Link className='mr-2 sm:mr-6' to='/'>Home</Link>
+
+                <Link className='mr-2 sm:mr-6 hidden sm:block' to='/about-us'>About Us</Link>
+
+                <Link className='mr-2 sm:mr-6' to='/rooms'>Rooms</Link>
+
+                <Link className='mr-2 sm:mr-6 hidden sm:block' to='/contact'>Contact</Link>
+
+                <Link className='mr-2 sm:mr-6' to='/login'>Login</Link>
+
+                <Link className='mr-2 sm:mr-6' to='/register'>Register</Link>
             </nav>
         </header>
     );
